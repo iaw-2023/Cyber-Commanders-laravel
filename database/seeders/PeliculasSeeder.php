11dbad4016e39946d4a423d08fab99da212be39a
Federicotalmon <54511081+Federicotalmon@users.noreply.github.com>
2023-04-11 12:46:14 -0300

    Creacion de seeder de peliculas

--- database/seeders/PeliculasSeeder.php
+++ database/seeders/PeliculasSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,13 @@ class PeliculasSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $peliculas = [
+            ['duracion'=> 175,'nombre'=>'el padrino', 'poster' => 'https://www.omdb.org/image/default/512.jpeg'],
+            ['duracion'=> 200,'nombre'=>'el padrino 2', 'poster' => 'https://www.omdb.org/image/default/516.jpeg'],
+            ['duracion'=> 162,'nombre'=>'el padrino 3', 'poster' => 'https://www.omdb.org/image/default/523.jpeg']
+        ];
+        
+        DB::table('peliculas')->insert($peliculas);
+
     }
 }
