e1d40faeb5bc53453ff55e813862d2515b54f016
Federicotalmon <54511081+Federicotalmon@users.noreply.github.com>
2023-04-22 18:58:38 -0300

    Creacion de algunos modelos
    Todavia no estan bien, pero los voy subiendo

--- app/Models/Pelicula.php
+++ app/Models/Pelicula.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pelicula extends Model
 {
     use HasFactory;
+    protected $table = 'peliculas' ;
+
+    public function funciones(): HasMany
+    {
+        return $this->hasMany(Funcion::class);
+    }
 }
